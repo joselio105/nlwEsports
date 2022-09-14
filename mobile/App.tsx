13df1aedc63fa1832497a0184846399879c24996
José Hélio Veríssimoo Jr <joselio105@users.noreply.github.com>
2022-09-14 11:16:40 -0300

    Aula 02

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,33 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface ButtonProps {
-  title: String
-}
-
-const Button = (props:ButtonProps) => {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontesLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Button title="Enviar"></Button>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar 
+        backgroundColor='transparent'
+        barStyle='light-content'
+        translucent
+      />
+      { fontesLoaded ? <Home /> : <Loading /> }
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#909000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,72 @@
-interface ButtonProps {
-  title: string
-}
+import "./styles/main.css";
+import { MagnifyingGlassPlus } from "phosphor-react";
 
-const Button = (props:ButtonProps) => {
-  const title = props.title;
-  return <button>{title}</button>
-}
+import logo from "./assets/Logo.svg";
 
 function App() {
   return (
-    <>
-      <Button title="Enviar" />
-      <Button title="Excluir" />
-      <Button title="Criar" />
-      <Button title="Voltar" />
-      <Button title="Atualizar" />
-    </>
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+      <img src={logo} alt="logo" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 1.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 2.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">DOTA 2</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 3.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 5.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 6.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnine</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 7.jpg" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Word of Warcraft</strong>
+            <span className="text-zinc-300 text-sm mt-1 block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+      <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="px-8 py-6 bg-[#2A2634] flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="px-3 py-4 text-white bg-violet-500 hover:bg-violet-600 rounded flex justify-between items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+    </div>
    
   )
 }
